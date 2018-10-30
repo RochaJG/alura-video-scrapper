10e6d15c13a26009740b1bf4438184f0b7b682a8
ceelsoin <celso.almeida@seredesa.com.br>
2018-10-30 18:02:06 -0300

    Algumas melhorias na estabilidade e adicionado download das aulas em texto

--- scrap.js
+++ scrap.js
@@ -1,12 +1,19 @@
 const puppeteer = require('puppeteer');
 var fs = require('fs');
 const Axios = require('axios')
-var stayAwake = require('stay-awake');
+const args = require('yargs').argv;
+const mkdirp = require('mkdirp');
 
 var username;
 var password;
+var HANDLESS = true;
+var firstRun = true
 
-const GLOBAL_CONFIG = {ignoreHTTPSErrors:true, headless: true};
+if(args.onlyCLI != "" && args.onlyCLI != null){
+  HANDLESS = args.onlyCLI;
+}
+
+const GLOBAL_CONFIG = {ignoreHTTPSErrors:true, headless: false};
 
 const USERNAME_SELECTOR = '#login-email';
 const PASSWORD_SELECTOR = '#password';
@@ -19,6 +26,7 @@ var COUNT_AULAS = 0;
 var TOTAL_AULAS = 0;
 
 var QUEUE_ATIVIDADES = [];
+var QUEUE_TIPO_ATIVIDADES = [];
 var COUNT_ATIVIDADES = 0;
 var TOTAL_ATIVIDADES = 0;
 
@@ -31,6 +39,90 @@ var COUNT_OPERATION;
 var LIST_OPERATION = [];
 var TOTAL_OPERATION;
 
+function slugfy (str) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "àáãäâèéëêìíïîòóöôùúüûñç·/_,:; ";
+  var to   = "aaaaaeeeeiiiioooouuuunc-------";
+
+  for (var i=0, l=from.length ; i<l ; i++) {
+      str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+      .replace(/\s+/g, '-') // collapse whitespace and replace by -
+      .replace(/-+/g, '-'); // collapse dashes
+
+  return str;
+}
+
+
+async function saveState(){
+
+  var fs = require('fs');
+
+  await fs.unlink('saveState.db', (err) => {
+    //if (err) throw err;
+  });
+
+  await fs.unlink('QUEUE_AULAS.db', (err) => {
+    //if (err) throw err;
+  });
+
+  await fs.unlink('QUEUE_TIPO_ATIVIDADES.db', (err) => {
+    //if (err) throw err;
+  });
+
+  await fs.unlink('QUEUE_ATIVIDADES.db', (err) => {
+    //if (err) throw err;
+  });
+  await fs.unlink('LIST_OPERATION.db', (err) => {
+    //if (err) throw err;
+  });
+
+  console.log("[STATUS]: Salvando estado da aplicação")
+  await fs.writeFile("saveState.db", COUNT_AULAS+"|"+TOTAL_AULAS+"|"+COUNT_ATIVIDADES+"|"+TOTAL_ATIVIDADES+"|"+COUNT_OPERATION+"|"+TOTAL_OPERATION+"|"+TOTAL_ESCRITAS, function(erro) {
+
+      if(erro) {
+          throw erro;
+      }
+      //console.log("[STATUS]: SAVESTATE.DB salvo com sucesso");
+  }); 
+
+ /* for(var i in QUEUE_AULAS){
+    await fs.appendFile('QUEUE_AULAS.db', QUEUE_AULAS[i]+"\r\n", function (err) {
+      if (err) {
+        console.log("[ERROR]: SAVE_STATE(QUEUE_AULAS): "+ err);
+      } else {
+       // console.log("[STATUS]: QUEUE_AULAS salvo com sucesso");
+      }
+    })
+  }
+
+  for(var i in QUEUE_ATIVIDADES){
+    await fs.appendFile('QUEUE_ATIVIDADES.db', QUEUE_ATIVIDADES[i]+"|"+QUEUE_TIPO_ATIVIDADES[i]+"\r\n", function (err) {
+      if (err) {
+        console.log("[ERROR]: SAVE_STATE(QUEUE_ATIVIDADES): "+ err);
+      } else {
+        //console.log("[STATUS]: QUEUE_ATIVIDADES salvo com sucesso");
+      }
+    })
+  }
+
+  for(var i in LIST_OPERATION){
+     await fs.appendFile('LIST_OPERATION.db', LIST_OPERATION[i]+"\r\n", function (err) {
+      if (err) {
+        console.log("[ERROR]: SAVE_STATE(LIST_OPERATION): "+ err);
+      } else {
+        //console.log("[STATUS]: LIST_OPERATION salvo com sucesso");
+      }
+    })
+  }*/
+
+}
+
 
 async function run() {
   QUEUE_AULAS = [];
@@ -38,14 +130,81 @@ async function run() {
   TOTAL_AULAS = 0;
 
   QUEUE_ATIVIDADES = [];
+  QUEUE_TIPO_ATIVIDADES = [];
+
   COUNT_ATIVIDADES = 0;
   TOTAL_ATIVIDADES = 0;
+  TOTAL_ESCRITAS = 0;
 
   LIST_VIDEOS = [];
   COUNT_VIDEOS = 0
   TOTAL_VIDEOS = 0;
 
-  scrap_aulas();
+  if(args.snapshot == 1 && firstRun == true){
+    var saveState = require('readline').createInterface({
+      input: require('fs').createReadStream('saveState.db')
+    });
+    
+    saveState.on('line', function (line) {
+      var db = line.split("|")
+      console.log('[STATUS]: Recuperando estado anterior...')
+
+      COUNT_AULAS = db[0];
+     /* TOTAL_AULAS = db[1];
+      COUNT_ATIVIDADES = db[2];
+      TOTAL_ATIVIDADES = db[3]; 
+      COUNT_OPERATION = db[4];
+      TOTAL_OPERATION = db[5];
+      TOTAL_ESCRITAS = db[6];*/
+      
+      console.log('COUNT_AULAS: '+ COUNT_AULAS);
+      /*console.log('TOTAL_AULAS: '+ TOTAL_AULAS);
+      console.log('COUNT_ATIVIDADES: '+ COUNT_ATIVIDADES);
+      console.log('TOTAL_ATIVIDADES: '+ TOTAL_ATIVIDADES);
+      console.log('COUNT_OPERATION: '+ COUNT_OPERATION);
+      console.log('TOTAL_OPERATION: '+ TOTAL_OPERATION);
+      console.log('TOTAL_ESCRITAS: ' + TOTAL_ESCRITAS);*/
+
+    });
+
+   /*var QUEUE_AULAS_DB = require('readline').createInterface({
+      input: require('fs').createReadStream('QUEUE_AULAS.db')
+    });
+
+    QUEUE_AULAS_DB.on('line', function (line) {
+      QUEUE_AULAS.push(line);
+    });
+
+    var QUEUE_TIPO_ATIVIDADES_DB = require('readline').createInterface({
+      input: require('fs').createReadStream('QUEUE_AULAS.db')
+    });
+
+    QUEUE_TIPO_ATIVIDADES_DB.on('line', function (line) {
+      QUEUE_TIPO_ATIVIDADES.push(line);
+    });
+
+    var QUEUE_ATIVIDADES_DB = require('readline').createInterface({
+      input: require('fs').createReadStream('QUEUE_AULAS.db')
+    });
+
+    QUEUE_ATIVIDADES_DB.on('line', function (line) {
+      QUEUE_ATIVIDADES.push(line);
+    });
+
+    var LIST_OPERATION_DB = require('readline').createInterface({
+      input: require('fs').createReadStream('QUEUE_AULAS.db')
+    });
+
+    LIST_OPERATION_DB.on('line', function (line) {
+      LIST_OPERATION.push(line);
+    });*/
+    firstRun = false;
+    scrap_aulas();
+  }else{
+    scrap_aulas();
+  }
+
+  
 }
 
 async function scrap_aulas(){
@@ -56,13 +215,14 @@ async function scrap_aulas(){
   
 
   if(OPERATION_MODE == 'single'){
-    await page.goto(args.course);
+    await page.goto(args.course, {waitUntil: 'domcontentloaded'});
     URL_COURSE = args.course;
   }else if(OPERATION_MODE == 'list'){
-    await page.goto(LIST_OPERATION[COUNT_OPERATION]);
+    await page.goto(LIST_OPERATION[COUNT_OPERATION], {waitUntil: 'domcontentloaded'});
     URL_COURSE = LIST_OPERATION[COUNT_OPERATION];
     
     COUNT_OPERATION++;
+    saveState();
   }
 
   console.log("[STATUS]: Acessando a página do curso: " + URL_COURSE);
@@ -99,7 +259,7 @@ async function scrap_atividades(){
 
   console.log('[STATUS]: Acessando aula: ' + QUEUE_AULAS[COUNT_AULAS]);
 
-  await page.goto(QUEUE_AULAS[COUNT_AULAS]);
+  await page.goto(QUEUE_AULAS[COUNT_AULAS], {waitUntil: 'domcontentloaded'});
 
   await page.click(USERNAME_SELECTOR);
   await page.keyboard.type(username);
@@ -124,19 +284,27 @@ async function scrap_atividades(){
 
   let atividades = await page.evaluate((sel) => {
     return Array.from(document.getElementsByClassName(sel)).map(node => node.href);
-  }, 'task-menu-nav-item-link-VIDEO')
+  }, 'task-menu-nav-item-link')
+
+  let tipo_atividades = await page.evaluate((sel) => {
+    return Array.from(document.getElementsByClassName(sel)).map(node => node.className);
+  }, 'task-menu-nav-item-link')
 
   for(var i in atividades) {
     QUEUE_ATIVIDADES.push(atividades[i]);
+    QUEUE_TIPO_ATIVIDADES.push(tipo_atividades[i]);
+    //console.log(atividades[i]);
   }
+
   TOTAL_ATIVIDADES = QUEUE_ATIVIDADES.length;
-  console.log('[STATUS]: Total de Aulas encontradas: ' + TOTAL_ATIVIDADES);
+  console.log('[STATUS]: Total deatividades encontradas: ' + TOTAL_ATIVIDADES);
 
   browser.close();
   scrap_video();
 
   if(COUNT_ATIVIDADES >= TOTAL_ATIVIDADES){
     COUNT_AULAS++;
+    saveState();
     //QUEUE_AULAS = [];
   }
 
@@ -144,19 +312,19 @@ async function scrap_atividades(){
 
 async function scrap_video(){
 
-  stayAwake.prevent(function() {}); // prevent system sleep
-
   const browser = await puppeteer.launch(GLOBAL_CONFIG); //Without proxy
   //const browser = await puppeteer.launch({args:[ '--proxy-server=http://contwebprd17:82',], ignoreHTTPSErrors:true, headless: false});
   const page = await browser.newPage();
 
   if(COUNT_ATIVIDADES >= TOTAL_ATIVIDADES){
     COUNT_AULAS++;
+    saveState();
     scrap_atividades();
+    
   }else{
     console.log('[STATUS]: Acessando aula: ' + QUEUE_ATIVIDADES[COUNT_ATIVIDADES]);
 
-    await page.goto(QUEUE_ATIVIDADES[COUNT_ATIVIDADES]);
+    await page.goto(QUEUE_ATIVIDADES[COUNT_ATIVIDADES], {waitUntil: 'domcontentloaded'});
 
     await page.click(USERNAME_SELECTOR);
     await page.keyboard.type(username);
@@ -167,66 +335,117 @@ async function scrap_video(){
     console.log("[STATUS]: Tentando fazer login...");
     await page.click(BUTTON_SELECTOR);
 
-    console.log('[STATUS]: Obtendo o URL do vídeo...');
 
-    try{
-      await page.waitForNavigation({timeout:30000}).then(()=>{},()=>{
-        console.log('[WARNING]: URL TIMEOUT Tentando pegar conteudo da página...');
-      });
-    }catch(e){
-      scrap_video();
-    }
-    
+    if(QUEUE_TIPO_ATIVIDADES[COUNT_ATIVIDADES] == "task-menu-nav-item-link task-menu-nav-item-link-VIDEO"){
+      console.log('[STATUS]: Obtendo o URL do vídeo...');
 
-    try {
-      const video = await page.evaluate(() => document.querySelector('#video-player-frame_html5_api > source:nth-child(1)').src);
-      LIST_VIDEOS.push(video);
-      console.log("[STATUS]: Video encontrado "/*+video*/);
-
-      var final_name = video.split('/')[4];
-
-      real_aula = COUNT_AULAS + 1;
-
-      real_atividades = COUNT_ATIVIDADES + 1;
-
-      var file_path = __dirname+"/downloads/"+URL_COURSE.split('/')[4]+"/Aula-"+real_aula;
-
-      var mkdirp = await require('mkdirp');
-      mkdirp(file_path, function(err) {});
-
-      var path = file_path+"/Video-"+real_atividades+"-"+final_name.split('=')[5];
-
-      console.log("[STATUS]: Baixando Video: " + final_name.split('=')[5]);
-      
-      await downloadVideo(path, video);
-
-      await browser.close();
-      
-      var FIX_TOTAL_AULAS = TOTAL_AULAS - 1; //Fixa total de aulas de acordo com contagem do array, evitando sobra de 1 elemento.
-      
-      //console.log('[STATUS]: ATIVIDADES: '+COUNT_ATIVIDADES+'/'+TOTAL_ATIVIDADES + ' AULAS: '+COUNT_AULAS+'/'+TOTAL_AULAS);
-      
-      if(COUNT_AULAS >= FIX_TOTAL_AULAS && COUNT_ATIVIDADES >= TOTAL_ATIVIDADES){
-        if(OPERATION_MODE == 'single'){
-          console.log('[STATUS]: Nada para fazer, tarefas concluidas!');
-          process.exit();        
-        }else if(OPERATION_MODE == 'list'){
-          if(COUNT_OPERATION >= TOTAL_OPERATION){
-            console.log('[STATUS]: Nada para fazer, tarefas concluidas!');
-            process.exit();   
-          }else{
-            run();
-          }
-        }
-        
-      }else{
+      try{
+        await page.waitForNavigation({timeout:50000}).then(()=>{},()=>{
+          console.log('[WARNING]: URL TIMEOUT Tentando pegar conteudo da página...');
+        });
+      }catch(e){
         scrap_video();
       }
-     
-    } catch (e) {
-      console.log('[ERROR] : Não foi possivel obter o vídeo, tentando novamente... ');
-      scrap_video();
+    
+      try {
+        const video = await page.evaluate(() => document.querySelector('#video-player-frame_html5_api > source:nth-child(1)').src);
+        LIST_VIDEOS.push(video);
+        console.log("[STATUS]: Video encontrado "+video);
+  
+        var final_name = slugfy(video.split('/')[4]);
+  
+        real_aula = COUNT_AULAS + 1;
+  
+        real_atividades = COUNT_ATIVIDADES + 1;
+  
+        var file_path = __dirname+"/downloads/"+URL_COURSE.split('/')[4]+"/Aula-"+real_aula;
+  
+        mkdirp(file_path, function(err) {});
+  
+        var path = file_path+"/atividade-"+real_atividades+"-"+final_name.split('=')[5];
+  
+        console.log("[STATUS]: Baixando Video: " + final_name.split('=')[5]);
+        
+        await downloadVideo(path, video);
+  
+        await browser.close();
+       
+      } catch (e) {
+        console.log('[ERROR] : Não foi possivel obter o vídeo, tentando novamente... ');
+        await browser.close();
+        scrap_video();
+      }
+    }else{//Aqui é caso a atividade seja um texto ou outra coisa que não seja vídeo
+      try{
+        await page.waitForNavigation({timeout:50000}).then(()=>{},()=>{
+          console.log('[WARNING]: URL TIMEOUT Tentando pegar conteudo da página...');
+        });
+      }catch(e){
+        scrap_video();
+      }
+    
+      try {
+        
+        const title = await page.evaluate(() => document.querySelector('.task-body-header-title-text').innerHTML);
+
+        const html = await page.evaluate(() => document.querySelector('.task-body').innerHTML);
+  
+        real_aula = COUNT_AULAS + 1;
+  
+        real_atividades = COUNT_ATIVIDADES + 1;
+
+        var final_name = "atividade-"+real_atividades+"-"+slugfy(title);
+  
+        var file_path = __dirname+"/downloads/"+URL_COURSE.split('/')[4]+"/Aula-"+real_aula;
+        console.log(html)
+  
+        mkdirp(file_path, function(err) {if(err){console.log(err)}});
+  
+        var path = file_path+"/"+final_name;
+  
+        console.log("[STATUS]: Baixando HTML: " + final_name);
+
+        // await fs.writeFile(encodeURI(path)+".html", html, function(erro) {
+
+        //   if(erro) {
+        //       throw erro;
+        //       scrap_video();
+        //   }
+    
+        //   console.log("[STATUS]: Arquivo HTML Salvo com sucesso");
+        //   COUNT_ATIVIDADES++;
+        //   saveState();
+        // }); 
+  
+        await browser.close();
+       
+      } catch (e) {
+        console.log('[ERROR] : Não foi possivel obter o HTML, tentando novamente... ');
+        scrap_video();
+      }
     }
+
+        var FIX_TOTAL_AULAS = TOTAL_AULAS - 1; //Fixa total de aulas de acordo com contagem do array, evitando sobra de 1 elemento.
+        
+        console.log('[STATUS]: CURSOS:'+COUNT_OPERATION+'/'+TOTAL_OPERATION+' ATIVIDADES: '+COUNT_ATIVIDADES+'/'+TOTAL_ATIVIDADES + ' AULAS: '+COUNT_AULAS+'/'+TOTAL_AULAS);
+        
+        if(COUNT_AULAS >= FIX_TOTAL_AULAS && COUNT_ATIVIDADES >= TOTAL_ATIVIDADES){
+            if(OPERATION_MODE == 'single'){
+              console.log('[STATUS]: Nada para fazer, tarefas concluidas!');
+              process.exit();        
+            }else if(OPERATION_MODE == 'list'){
+              if(COUNT_OPERATION >= TOTAL_OPERATION){
+                console.log('[STATUS]: Nada para fazer, tarefas concluidas!');
+                process.exit();   
+              }else{
+                run();
+              }
+            }
+        }else{
+          scrap_video();
+        }
+
+    
   }
 
 }
@@ -246,6 +465,7 @@ async function downloadVideo(path, url) {
   return new Promise((resolve, reject) => {
     response.data.on('end', () => {
       COUNT_ATIVIDADES++;
+      saveState();
       resolve()
     })
 
@@ -254,11 +474,6 @@ async function downloadVideo(path, url) {
     })
   })
 }
-
-// prevent auto sleep 
-stayAwake.prevent(function(err, data) {
-  //console.log('%d routines are preventing sleep', data);
-});
 
 //run();
 
@@ -271,13 +486,15 @@ function helper(){
   console.log('              --course=https://cursos.alura.com.br/course/android-...');
   console.log('              OR');
   console.log('              --course=listOfCourses.txt');
+  console.log('              --snapshot=1');
+  console.log('              --onlyCLI=true');
 }
 
 
 
 'use strict';
 
-const args = require('yargs').argv;
+
 async function get_args(){
   if(args.username == null || args.username == "" || args.username == 'undefinied' || args.pass == null || args.pass == "" || args.pass == 'undefinied' || args.mode == null || args.mode == "" || args.mode == 'undefinied'){
     helper();
